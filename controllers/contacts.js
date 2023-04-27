6c9dfd3207f62dd9e936aef607d8333d5b3bdbec
trukkerweeler <ken22008@byui.edu>
2023-04-27 08:03:12 -0600

    l04_chasing-cors-swagger-recompile_0803

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -5,7 +5,6 @@ const mongodb = require('../db/connect');
 const getAll = async (req, res) => {
   const result = await mongodb.getDb().db('testdb').collection('contacts').find();
   result.toArray().then((lists) => {
-    // res.setHeader("Content-Type: text/html; charset=utf-8");
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
   });
@@ -15,7 +14,6 @@ const getOne = async (req, res) => {
   const userId = new ObjectId(req.params.id);
   const result = await mongodb.getDb().db('testdb').collection('contacts').find({ _id: userId });
   result.toArray().then((lists) => {
-    // res.setHeader("Content-Type: text/html; charset=utf-8");
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists[0]);
   })
--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    // res.setHeader("Content-Type: text/html; charset=utf-8");
     next();
   })
   .use("/contacts", contactRoutes)
